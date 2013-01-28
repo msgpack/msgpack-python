6a28b28c6314158ee099c95d452406278cdb93b0
Bas Westerbaan <bas@westerbaan.name>
2013-01-28 12:27:24 +0100

    Add pure Python fallback module
    Signed-off-by: Bas Westerbaan <bas@westerbaan.name>

--- msgpack/__init__.py
+++ msgpack/__init__.py
@@ -1,8 +1,12 @@
 # coding: utf-8
 from msgpack._version import version
 from msgpack.exceptions import *
-from msgpack._packer import pack, packb, Packer
-from msgpack._unpacker import unpack, unpackb, Unpacker
+
+try:
+    from msgpack._packer import pack, packb, Packer
+    from msgpack._unpacker import unpack, unpackb, Unpacker
+except ImportError:
+    from msgpack.fallback import pack, packb, Packer, unpack, unpackb, Unpacker
 
 # alias for compatibility to simplejson/marshal/pickle.
 load = unpack
