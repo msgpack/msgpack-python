cbabeebc95e9e42c0356e089b742588a4de75d56
INADA Naoki <inada-n@klab.com>
2013-01-29 14:47:16 +0900

    Use MSGPACK_PUREPYTHON envvar to test fallback module

--- msgpack/__init__.py
+++ msgpack/__init__.py
@@ -2,11 +2,15 @@
 from msgpack._version import version
 from msgpack.exceptions import *
 
-try:
-    from msgpack._packer import pack, packb, Packer
-    from msgpack._unpacker import unpack, unpackb, Unpacker
-except ImportError:
+import os
+if os.environ.get('MSGPACK_PUREPYTHON'):
     from msgpack.fallback import pack, packb, Packer, unpack, unpackb, Unpacker
+else:
+    try:
+        from msgpack._packer import pack, packb, Packer
+        from msgpack._unpacker import unpack, unpackb, Unpacker
+    except ImportError:
+        from msgpack.fallback import pack, packb, Packer, unpack, unpackb, Unpacker
 
 # alias for compatibility to simplejson/marshal/pickle.
 load = unpack
