95dfec808a700c99144e454fbf9d98be420f2c51
INADA Naoki <inada-n@klab.com>
2013-02-03 00:02:37 +0900

    Add simple benchmark.

---
+++ benchmark/benchmark.py
@@ -0,0 +1,32 @@
+from msgpack import fallback
+try:
+    from msgpack import _unpacker, _packer
+    has_ext = True
+except ImportError:
+    has_ext = False
+import timeit
+
+
+def profile(name, func):
+    times = timeit.repeat(func, number=1000, repeat=4)
+    times = ', '.join(["%8f" % t for t in times])
+    print("%-30s %40s" % (name, times))
+
+
+def simple(name, data):
+    if has_ext:
+        profile("packing %s (ext)" % name, lambda: _packer.packb(data))
+    profile('packing %s (fallback)' % name, lambda: fallback.packb(data))
+
+    data = fallback.packb(data)
+    if has_ext:
+        profile('unpacking %s (ext)' % name, lambda: _unpacker.unpackb(data))
+    profile('unpacking %s (fallback)' % name, lambda: fallback.unpackb(data))
+
+def main():
+    simple("integers", [7]*10000)
+    simple("bytes", [b'x'*n for n in range(100)]*10)
+    simple("lists", [[]]*10000)
+    simple("dicts", [{}]*10000)
+
+main()
