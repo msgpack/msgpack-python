43dd224d52eb4711c68101b40b910d252dacc290
INADA Naoki <inada-n@klab.com>
2013-02-21 16:04:58 +0900

    Fix test for Python 3.

--- test/test_reserved.py
+++ test/test_reserved.py
@@ -1,6 +1,20 @@
 import msgpack
 
-reserved_bytes = b"\xc1\xc4\xc5\xc6\xc7\xc8\xc9\xd4\xd5\xd6\xd7\xd8\xd9"
+reserved_bytes = [
+        b"\xc1",
+        b"\xc4",
+        b"\xc5",
+        b"\xc6",
+        b"\xc7",
+        b"\xc8",
+        b"\xc9",
+        b"\xd4",
+        b"\xd5",
+        b"\xd6",
+        b"\xd7",
+        b"\xd8",
+        b"\xd9",
+        ]
 
 def test_skip_reserved():
     packed_list = msgpack.packb([])
