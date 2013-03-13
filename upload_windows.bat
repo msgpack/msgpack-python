5c90f953dae37610f478183849b77413d6ec8e9f
INADA Naoki <inada-n@klab.com>
2013-03-13 18:15:43 +0900

    Add build batch for Windows

---
+++ upload_windows.bat
@@ -0,0 +1,4 @@
+c:\Python27\python       setup.py bdist_egg bdist_wininst upload
+c:\Python33\python       setup.py bdist_egg bdist_wininst upload
+c:\Python27_amd64\python setup.py bdist_egg bdist_wininst upload
+c:\Python33_amd64\python setup.py bdist_egg bdist_wininst upload
