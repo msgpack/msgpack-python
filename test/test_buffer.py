518f886b111808d7d9f29b34c50baddb01610338
INADA Naoki <inada-n@klab.com>
2014-02-17 04:06:58 +0900

    fix

--- test/test_buffer.py
+++ test/test_buffer.py
@@ -8,7 +8,7 @@ import sys
 def test_unpack_buffer():
     from array import array
     buf = array('b')
-    buf.fromstring(packb(('foo', 'bar')))
+    buf.fromstring(packb((b'foo', b'bar')))
     obj = unpackb(buf, use_list=1)
     assert [b'foo', b'bar'] == obj
 
@@ -17,7 +17,5 @@ def test_unpack_bytearray():
     buf = bytearray(packb(('foo', 'bar')))
     obj = unpackb(buf, use_list=1)
     assert [b'foo', b'bar'] == obj
-    expected_type = bytes if sys.version_info[0] == 3 else str
-    assert all(type(s)==expected_type for s in obj)
-
-
+    expected_type = bytes
+    assert all(type(s) == expected_type for s in obj)
