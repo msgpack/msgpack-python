e76091a82c04d092a363b0e1f7307dc70bf784f5
Inada Naoki <songofacandy@gmail.com>
2018-11-29 22:38:22 +0900

    Fix test

--- test/test_except.py
+++ test/test_except.py
@@ -55,9 +55,9 @@ def test_invalidvalue():
 def test_strict_map_key():
     valid = {u"unicode": 1, b"bytes": 2}
     packed = packb(valid, use_bin_type=True)
-    assert valid == unpackb(packed, raw=True)
+    assert valid == unpackb(packed, raw=True, strict_map_key=True)
 
     invalid = {42: 1}
     packed = packb(invalid, use_bin_type=True)
     with raises(ValueError):
-        unpackb(packed, raw=True)
+        unpackb(packed, raw=True, strict_map_key=True)
