b8bf3c950c4474d3af82a6b6bda6326b2e197a5e
Inada Naoki <songofacandy@gmail.com>
2018-12-04 17:18:34 +0900

    Build linux wheel for Python 3.7

--- docker/buildwheel.sh
+++ docker/buildwheel.sh
@@ -4,7 +4,7 @@ set -e -x
 ARCH=`uname -p`
 echo "arch=$ARCH"
 
-for V in cp36-cp36m cp35-cp35m cp27-cp27m cp27-cp27mu; do
+for V in cp37-cp37m cp36-cp36m cp35-cp35m cp27-cp27m cp27-cp27mu; do
     PYBIN=/opt/python/$V/bin
     rm -rf build/       # Avoid lib build by narrow Python is used by wide python
     $PYBIN/python setup.py bdist_wheel -p manylinux1_${ARCH}
