4efe5e0726fb3622b59d26c8c965ea548a7567eb
Inada Naoki <songofacandy@gmail.com>
2020-02-17 18:35:04 +0900

    fix

--- docker/buildwheel.sh
+++ docker/buildwheel.sh
@@ -10,7 +10,6 @@ echo "arch=$ARCH"
 for V in "${PYTHON_VERSIONS[@]}"; do
     PYBIN=/opt/python/$V/bin
     rm -rf build/       # Avoid lib build by narrow Python is used by wide python
-    #$PYBIN/python setup.py bdist_wheel -p manylinux2010_${ARCH}
     $PYBIN/python setup.py bdist_wheel
 done
 
