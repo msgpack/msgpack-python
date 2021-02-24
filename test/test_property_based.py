df1c2dd25116607c9cdc592b38751e13bc633537
Andrey Bienkowski <hexagonrecursion@gmail.com>
2021-02-24 12:10:28 +0300

    Start testing with hypothesis

---
+++ test/test_property_based.py
@@ -0,0 +1,24 @@
+import pytest
+from hypothesis import given
+from hypothesis import strategies as st
+
+try:
+    from msgpack import _cmsgpack
+except ImportError:
+    _cmsgpack = None
+from msgpack import fallback
+
+# https://github.com/msgpack/msgpack/blob/master/spec.md#type-system
+simple_types = (
+    st.none()
+    | st.booleans()
+)
+
+
+@pytest.mark.skipif(_cmsgpack is None, reason='C extension is not available')
+@given(simple_types)
+def test_extension_and_fallback_pack_identically(obj):
+    extension_packer = _cmsgpack.Packer()
+    fallback_packer = fallback.Packer()
+
+    assert extension_packer.pack(obj) == fallback_packer.pack(obj)
