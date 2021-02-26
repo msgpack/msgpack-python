ae209fb1a2fb8fa1eb2300727208f58616dabbee
Andrey Bienkowski <hexagonrecursion@gmail.com>
2021-02-26 10:07:14 +0300

    Add PR link

--- test/test_discrepancy.py
+++ test/test_discrepancy.py
@@ -12,7 +12,10 @@ except ImportError:
 def test_exceed_max_buffer_size(use_unpack):
     """The C extension used to have a bug: when reading objects that require
     a buffer bigger than max_buffer_size it would behave as if there is not
-    enough data"""
+    enough data
+
+    See https://github.com/msgpack/msgpack-python/pull/464#issuecomment-786457374
+    """
     buffer_size = 11
     max_buffer_size = 10
     f = io.BytesIO(b"\xc6" + buffer_size.to_bytes(4, "big") + b"z" * buffer_size)
