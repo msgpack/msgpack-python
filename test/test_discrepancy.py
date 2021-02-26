5680c1f2bbe06017372874557f287c5a5c9dae5b
Andrey Bienkowski <hexagonrecursion@gmail.com>
2021-02-26 10:17:37 +0300

    Show: now fallback and extension behave the same

--- test/test_discrepancy.py
+++ test/test_discrepancy.py
@@ -1,5 +1,7 @@
 import io
-from pytest import raises, mark
+from pytest import raises, mark, skip
+
+from msgpack import fallback
 
 try:
     from msgpack import _cmsgpack
@@ -7,19 +9,22 @@ except ImportError:
     _cmsgpack = None
 
 
-@mark.skipif(_cmsgpack is None, reason="C extension not awailable")
+@mark.parametrize("impl", [fallback, _cmsgpack])
 @mark.parametrize("use_unpack", [True, False])
-def test_exceed_max_buffer_size(use_unpack):
+def test_exceed_max_buffer_size(impl, use_unpack):
     """The C extension used to have a bug: when reading objects that require
     a buffer bigger than max_buffer_size it would behave as if there is not
     enough data
 
     See https://github.com/msgpack/msgpack-python/pull/464#issuecomment-786457374
     """
+    if impl is None:
+        skip("C extension not awailable")
+
     buffer_size = 11
     max_buffer_size = 10
     f = io.BytesIO(b"\xc6" + buffer_size.to_bytes(4, "big") + b"z" * buffer_size)
-    u = _cmsgpack.Unpacker(f, max_buffer_size=max_buffer_size)
+    u = impl.Unpacker(f, max_buffer_size=max_buffer_size)
 
     with raises(ValueError):
         if use_unpack:
