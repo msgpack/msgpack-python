9622b83fdeb8891ebf25441665de09e0144d8b55
Andrey Bienkowski <hexagonrecursion@gmail.com>
2021-03-01 13:10:08 +0300

    Add test dependency: hypothesis

--- docker/runtests.sh
+++ docker/runtests.sh
@@ -8,7 +8,7 @@ for V in "${PYTHON_VERSIONS[@]}"; do
     PYBIN=/opt/python/$V/bin
     $PYBIN/python setup.py install
     rm -rf build/       # Avoid lib build by narrow Python is used by wide python
-    $PYBIN/pip install pytest
+    $PYBIN/pip install pytest hypothesis
     pushd test          # prevent importing msgpack package in current directory.
     $PYBIN/python -c 'import sys; print(hex(sys.maxsize))'
     $PYBIN/python -c 'from msgpack import _cmsgpack'  # Ensure extension is available
