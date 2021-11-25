e22cc52c495914c8a6aa14279425ca4b9fbf9da5
Inada Naoki <songofacandy@gmail.com>
2021-11-25 11:54:24 +0900

    remove ci/runtests.sh

--- ci/runtests.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -ex
-${PYTHON} -VV
-${PYTHON} -m pip install setuptools wheel pytest build
-${PYTHON} setup.py build_ext -if
-${PYTHON} -c "from msgpack import _cmsgpack"
-${PYTHON} -m pytest -v test
-${PYTHON} -m build
