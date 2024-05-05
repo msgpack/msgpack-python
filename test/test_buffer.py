04ddc1bdf69a005755f621efa18b1b0a29d1f5e0
Inada Naoki <songofacandy@gmail.com>
2024-05-06 03:10:06 +0900

    fix test

--- test/test_buffer.py
+++ test/test_buffer.py
@@ -34,12 +34,14 @@ def test_packer_getbuffer():
     packer.pack_array_header(2)
     packer.pack(42)
     packer.pack("hello")
-    assert bytes(packer) == b"\x92*\xa5hello"
     buffer = packer.getbuffer()
     assert isinstance(buffer, memoryview)
     assert bytes(buffer) == b"\x92*\xa5hello"
 
     if Packer.__module__ == "msgpack._cmsgpack":  # only for Cython
+        # cython Packer supports buffer protocol directly
+        assert bytes(packer) == b"\x92*\xa5hello"
+
         with raises(BufferError):
             packer.pack(42)
         buffer.release()
