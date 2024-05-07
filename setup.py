08e52ab9e4a9c4c98aa3d8a0dbe3721edbb2dbb0
Inada Naoki <songofacandy@gmail.com>
2024-05-07 21:43:25 +0900

    do not install cython as build dependency

--- setup.py
+++ setup.py
@@ -8,47 +8,6 @@ from setuptools.command.sdist import sdist
 
 PYPY = hasattr(sys, "pypy_version_info")
 
-
-class NoCython(Exception):
-    pass
-
-
-try:
-    import Cython.Compiler.Main as cython_compiler
-
-    have_cython = True
-except ImportError:
-    have_cython = False
-
-
-def cythonize(src):
-    if not have_cython:
-        raise Exception("Cython is required for building from checkout")
-    sys.stderr.write(f"cythonize: {src!r}\n")
-    cython_compiler.compile([src])
-
-
-def ensure_source(src):
-    pyx = os.path.splitext(src)[0] + ".pyx"
-
-    if not os.path.exists(src) or have_cython and os.stat(src).st_mtime < os.stat(pyx).st_mtime:
-        cythonize(pyx)
-
-
-class BuildExt(build_ext):
-    def build_extension(self, ext):
-        for src in ext.sources:
-            ensure_source(src)
-        return build_ext.build_extension(self, ext)
-
-
-# Cython is required for sdist
-class Sdist(sdist):
-    def __init__(self, *args, **kwargs):
-        cythonize("msgpack/_cmsgpack.pyx")
-        sdist.__init__(self, *args, **kwargs)
-
-
 libraries = []
 macros = []
 ext_modules = []
@@ -69,9 +28,7 @@ if not PYPY and not os.environ.get("MSGPACK_PUREPYTHON"):
     )
 del libraries, macros
 
-
 setup(
-    cmdclass={"build_ext": BuildExt, "sdist": Sdist},
     ext_modules=ext_modules,
     packages=["msgpack"],
 )
